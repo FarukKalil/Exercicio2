a1c6731c4bae11063ca98ff4cab564ee58841e76
FarukKalil <faruk1.kalil@gmail.com>
2023-03-20 10:43:25 -0300

    Exercicio 2 finalizado

--- Exercicio2/src/exercicio2/Exercicio2.java
+++ Exercicio2/src/exercicio2/Exercicio2.java
@@ -1,9 +1,19 @@
 package exercicio2;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
 
     public static void main(String[] args) {
-        
+                double n1, n2, soma;
+            Scanner captura = new Scanner (System.in);
+        System.out.println("Digite um número: ");
+            n1 = captura.nextDouble();
+            System.out.println("Digite outro número: ");
+            n2 = captura.nextDouble();
+            
+            soma = n1 + n2;
+            
+            System.out.println("A soma de "+n1+" + "+n2+" é"+soma);
     }
-    
 }
